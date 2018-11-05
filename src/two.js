6985fb9ba0f407f8ee4021552c56a8bddee4d0bd
dreamyaojing <1697261314@qq.com>
2018-11-05 21:19:40 +0800

    mm

---
+++ src/two.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+export default class Big extends Component{
+    render(){
+        return(
+            <div>
+                <h2>
+                    醉里挑灯看剑，梦回吹角连营
+                </h2>
+            </div>
+        )
+    }
+}
