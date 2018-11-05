2ae7eefde1993bb95281584d3ee7a7dd6ba072a2
dreamyaojing <1697261314@qq.com>
2018-11-05 19:41:28 +0800

    no message

---
+++ src/one.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class Small extends Component{
+    render(){
+      return(
+        <div>
+          <section> 
+            <div className='shige'>
+              udiwdjwqoidjwq;odw
+            </div>
+            <p className='shige'>
+              他年我若帝，报与桃花
+            </p>
+          </section>
+        </div>
+      );
+    }
+  }
+  export default Small;
